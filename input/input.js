d5b52eaf4cf9c7304cf1f1b9919d6114469e627e
zanjiri <mamaxdobr@gmail.com>
2023-09-11 23:41:20 +0330

    count

--- input/input.js
+++ input/input.js
@@ -1,14 +1,22 @@
 let counter = 0;
 const kirelement = document.getElementById("des-placeholder-counter");
-kirelement.addEventListener("keydown", myFunction);
+kirelement.addEventListener("keydown", count);
 
-function myFunction() {
+/* function myFunction() {
   if (event.key && event.key !== "Backspace") {
+    counter++;
     console.log(event.key);
     console.log(counter);
-    counter++;
   } else if (event.key === "Backspace") {
     counter--;
     console.log(counter);
   }
+} */
+
+const mamad = document.getElementById("des-placeholder-counter").value;
+function count() {
+  for (i = 0; i < mamad.length; i++) {
+    console.log(mamad[i]);
+  }
+  console.log(mamad);
 }
